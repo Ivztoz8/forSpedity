84bd46218c4e92b5736889f14ceea74f9cab3a3f
Ivan <ivan.toselli31@gmail.com>
2022-10-18 16:55:01 +0200

    Half_work_complete

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        object: {
+            data: '14/10/2022',
+            sType: 'standard',
+            sFrom: '03039 - Sora (FR)',
+            sTo: '00030 - Vivaro Romano (RM)',
+            pallet: '100Kg 100x100x100 cm',
+            qta: '1',
+            offer: 'Nessuna offerta ricevuta',
+            deadline: 'Scade il 24/10/2022 alle 0:00',
+          }
+    }
+});
